9738e378fac21da581210fa7e3ab1b040858dcdf
jwidzer <jwidzer@homesnap.com>
2019-12-11 15:28:53 -0500

    aged brie quality 49

--- csharp/GildedRoseTest.cs
+++ csharp/GildedRoseTest.cs
@@ -14,5 +14,14 @@ namespace csharp
             app.UpdateQuality();
             Assert.AreEqual("foo", Items[0].Name);
         }
+
+        [Test]
+        public void agedBrieQuality49()
+        {
+            IList<Item> Items = new List<Item> { new Item { Name = "Aged Brie", SellIn = 0, Quality = 49 } };
+            GildedRose app = new GildedRose(Items);
+            app.UpdateQuality();
+            Assert.AreEqual("Aged Brie", Items[0].Name);
+        }
     }
 }
