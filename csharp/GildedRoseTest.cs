65b77e347bbb8a5326a8052cb45d7f616d6d6875
jwidzer <jwidzer@homesnap.com>
2019-12-11 15:49:24 -0500

    another test

--- csharp/GildedRoseTest.cs
+++ csharp/GildedRoseTest.cs
@@ -9,10 +9,11 @@ namespace csharp
         [Test]
         public void foo()
         {
-            IList<Item> Items = new List<Item> { new Item { Name = "foo", SellIn = 0, Quality = 0 } };
+            IList<Item> Items = new List<Item> { new Item { Name = "foo", SellIn = 10, Quality = 0 } };
             GildedRose app = new GildedRose(Items);
             app.UpdateQuality();
             Assert.AreEqual("foo", Items[0].Name);
+            Assert.AreEqual(9, Items[0].SellIn);
         }
 
         [Test]
